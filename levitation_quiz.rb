69bb6d47033b498a8ec2bd811787734f519e9729
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-05-28 00:53:31 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -7,3 +7,12 @@ def levitation_quiz
     puts "You passed the quiz!"
 end
 levitation_quiz
+
+def levitation_quiz
+  loop do
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa" 
+  end
+    puts "You passed the quiz!"
+end
